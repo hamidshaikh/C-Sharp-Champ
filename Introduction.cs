e6cf63d6eda007f80d6dbce640bca8b62ce3b975
hamidshaikh <hamids1193@gmail.com>
2019-12-19 22:42:42 +0530

    Update Introduction.cs

--- Introduction.cs
+++ Introduction.cs
@@ -7,7 +7,7 @@ using System;
 //'Program' is a name of class which is public in this example.
 public class Program
 {
-	// 'Main' method is a entry point of application, so here application starts from main method.
+  // 'Main' method is a entry point of application, so here application starts from main method.
   public static void Main()
   {
       //Console is a class present in System namespace and has a method 'WriteLine' to print message. i.e. Hello World.
