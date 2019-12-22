d35ec4fcf3483831feb725ef76278765726e822b
hamidshaikh <hamids1193@gmail.com>
2019-12-22 14:13:28 +0530

    Common Operator in C#
    This example is the demonstration of common operator in C#.

---
+++ 005 CO.cs
@@ -0,0 +1,57 @@
+//--------------------------------------------------------------------------------------
+	/*
+		Common operator in C#.
+		1) Assignment operator like =
+		2) Arithmatic operator like +, -, *, /, %
+		3) Comparison operator like ==, !=, >, >=, <, <=
+		4) Conditional operator like &&, ||
+		5) Ternary Operator like ?:
+		6) Null Coalesing Operator ??
+		
+		In this program we will see each operator example.
+	*/
+//--------------------------------------------------------------------------------------
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//--------------------------------------------------------------------------------------
+		// 1) Assignment operator : we are assigning value 10 to variable i.
+		//--------------------------------------------------------------------------------------
+		
+		int i = 10;
+		Console.WriteLine("i = {0}", i);
+		
+		//--------------------------------------------------------------------------------------
+		// 1) Arithmatic operator : Output of below code will be 10 / 2 = 5
+		//--------------------------------------------------------------------------------------
+		
+		int number = 10, devident = 2;
+		Console.WriteLine(number + " / " + devident + " = " + (number / devident));
+		
+		//--------------------------------------------------------------------------------------
+		// 1) Comparison operator : Output of below code will be '10 / 2 = 5 Is number 10 : true'
+		//--------------------------------------------------------------------------------------
+		
+		bool isNumber10 = false;
+		if(number == 10)
+			isNumber10 = true;
+		else
+			isNumber10 = false;
+		Console.WriteLine("Is number 10 : {0}", isNumber10);
+		
+		//--------------------------------------------------------------------------------------
+		// 1) Ternary Operator : Ternary operator also called short hand operator
+		//--------------------------------------------------------------------------------------
+		
+		number = 20;
+		isNumber10 = number== 10 ? true : false;
+		Console.WriteLine("Is number 10 : {0}", isNumber10);
+		
+		//--------------------------------------------------------------------------------------
+		// 1) Null Coalesing Operator : We will see in next example.
+		//--------------------------------------------------------------------------------------
+	}
+}
