586e03901c41212e49da92b0edfd3782b5aa527c
hamidshaikh <hamids1193@gmail.com>
2019-12-28 23:56:16 +0530

    for and foreach loop in C#.
    Demonstration of for and for each loop.

---
+++ 015 forAndForEachLoop.cs
@@ -0,0 +1,47 @@
+/*
+	for and foreach loop in C#.
+	
+	for is the looping statement like while loop.
+	
+	for and foreach loop is used for iteration but the one normal defferance is that,
+	whatever the class which implements the IEnumarable class internally is used with foreach loop.
+	
+	Otherwise we have to use for loop.
+	
+	Lets see it by example.
+*/
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] arr = new int[3];
+		
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		
+		//--------------------------------------------------------------------------------------
+		//Example of for loop.
+		//--------------------------------------------------------------------------------------'
+		
+		Console.WriteLine("Output by 'for' loop");
+		
+		for(int i = 0; i <= arr.Length-1; i++){
+			Console.WriteLine(arr[i]);
+		}
+		
+		//--------------------------------------------------------------------------------------
+		//Example of foreach loop
+		//Array internally implement IEnumarable loop so we can use forw=each loop on array.
+		//--------------------------------------------------------------------------------------
+		
+		Console.WriteLine("\nOutput by 'foreach' loop");
+		
+		foreach(var arrElement in arr){
+			Console.WriteLine(arrElement);
+		}
+	}
+}
