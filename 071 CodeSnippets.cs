fa94dce6a4a0941fa97920993b3c771333eec982
hamidshaikh <hamids1193@gmail.com>
2020-01-30 23:11:58 +0530

    Code snippets in Visual Studio.
    Code snippets and it;s shortcut.

---
+++ 071 CodeSnippets.cs
@@ -0,0 +1,33 @@
+//------------------------------------------------------------------------------------------------------------
+/*
+	Code snippets are ready made snippets of code you can quickly insert into your code.
+	
+	1. Keyboard shortcut: CTRL K + X
+	2. Right click and select "insert snippets" from the context menu.
+	3. Click on edit -> Intellisense -> Insert Snippets
+	4. Use code snippets short cut. For example to use "For Loop" code snippets, type "for" and press "TAB" key twice.
+	
+	Once code snippets inserted, the editable fields are highlighed in yellow, and the fiirst editable field selected automatically.
+	Upon changing the first editable field, press "TAB" to move next editable fields. To come to the privous editable fields use 
+	Shift + TAB. Press Enter or escsape keys to cancel field editiong and return the code editor to normal.
+	
+	* Code snippets type:
+	
+		1) Expansion: These snippets allow the code snippets to be inserted at the cursor.
+		
+		2) Surrounding with : These snippets allow the code snippets to be placed around the selected peice of code.
+		
+		3) Refactoring : These snippets are used during code refactoring.
+		
+	Surrounding with code snippets surround the code, with the code snippets code.
+	
+	1. Select the code to surround, and use the keyboard shortcut CLRT K+S
+	2. Select the code surround, right click and select "Surround with..." option from context menu.
+	3. Select the code to surround, then clickk on edit menu, "Select Intellisense" and then select the 
+	"Surround With" command.
+	
+	Code snippets can be used with any type of application that you create with visual studio
+	
+	Let's see the simple example...
+*/
+//------------------------------------------------------------------------------------------------------------
