924bf690f6dd48266258c4156df84199749464a1
hamidshaikh <hamids1193@gmail.com>
2020-02-05 00:19:15 +0530

    Anonymous function in C#.
    Anonymous function in C#.

---
+++ 098 AnonymousFunction.cs
@@ -0,0 +1,65 @@
+/*
+    Anonymous method in C#.
+	
+	What is anonymous method in C#?
+	Anonymous method is a method without a name. Introduced in C# 2. They provide us a way of creating delegates instances 
+	without having to write a sepates method.
+	
+	Let's try to understand it by example.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {	
+			List<Customer> listCustomer = new List<Customer>()
+			{
+				new Customer{ Id = 101, Name = "Mark"},
+				new Customer{ Id = 102, Name = "Mark"},
+				new Customer{ Id = 103, Name = "Mark"},
+			};
+			
+			//Step 2
+			//Predicate<Customer> customerPredicates = new Predicate<Customer>(FindEmployee);
+			
+			//Step 3
+			//Customer customer = listCustomer.Find(c => customerPredicates(c));
+			
+			Customer customer = listCustomer.Find(delegate(Customer c)
+												  {
+													  return c.Id == 102;
+												  });
+			
+			Console.WriteLine("Id = {0} Name = {1}", customer.Id, customer.Name);
+		}
+		
+		//Step 1
+		//public static bool FindEmployee(Customer customer)
+		//{
+			//return customer.Id == 102;
+		//}
+    }
+	
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+/*
+	Output :
+		Stack lenght 3
+		Customer Pop of Id 103
+		Stack lenght 2
+		Customer Peek() of Id 102
+		Stack lenght 2
+		102
+		101
+*/
